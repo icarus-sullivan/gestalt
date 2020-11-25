18a5f31cdb99ae894ee8f369e186bba9ec35980a
Chris Sullivan <chrissullivan.dev@gmail.com>
2020-11-25 13:09:12 -0800

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #Upload the built config to s3
 
+PROJECT_NAME=$( more package.json | jq -r '.name' )
+
 if [ -z ${NODE_ENV+x} ]; then 
     echo "NODE_ENV is not set"; 
     exit 1;
 fi
 
-aws s3 sync build "s3://gestalt-$NODE_ENV"
+aws s3 sync build "s3://$PROJECT_NAME-$NODE_ENV"
